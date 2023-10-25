b5b31a74fdab451f9257458d89b177ff752d7b23
John Gaitho <johngaitho05@gmail.com>
2023-10-26 00:04:49 +0300

    fizzbuzz

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -15,15 +15,17 @@ def fizzbuzz(n):
     if n < 1:
         return
 
+    tmp_result = []
     for i in range(1, n + 1):
         if (i % 15) == 0:
-            print("FizzBuzz")
+            tmp_result.append("FizzBuzz")
         elif (i % 3) == 0:
-            print("Fizz")
+            tmp_result.append("Fizz")
         elif (i % 5) == 0:
-            print("Buzz")
+            tmp_result.append("Buzz")
         else:
-            print(str(i))
+            tmp_result.append(str(i))
+    print(" ".join(tmp_result))
 
 
 if __name__ == '__main__':
